d87ba43ff9f9fb8c84facc6f1fd21f70af2b29ed
david900916 <39638467+david900916@users.noreply.github.com>
2018-07-06 19:35:44 +0800

    Add files via upload

---
+++ HOME.php
@@ -0,0 +1,8 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<form name="form" method="post"><CENTER>
+<a href="UserManager.php">使用者資料管理</a><P ALIGN=LEFT、CENTER、RIGHT>
+<a href="Point.php">積分管理</a><P ALIGN=LEFT、CENTER、RIGHT>
+<a href="UserQuery.php">使用者查詢</a><P ALIGN=LEFT、CENTER、RIGHT>
+<a href="Card.php">卡片</a><P ALIGN=LEFT、CENTER、RIGHT>
+<a href="index.php">首頁</a><P ALIGN=LEFT、CENTER、RIGHT></CENTER>
+</form>
--- adduser.php
+++ adduser.php
@@ -2,51 +2,28 @@
 <body>
 <h2>用戶新增</h2>
 <form action="" method="POST">
-    <input type="text" name="name" value="" placeholder="名稱" required>
-	<input type="text" name="account" value="" placeholder="帳號" required>
-    <input type="text" name="password" value="" placeholder="密碼" required>
-    <input type="submit" name="addButton" value="新增">
-	<input type="submit" name="deleteButton" value="刪除">
-	<input type="submit" name="reviseButton" value="修改">
-	<input type="submit" name="inquireButton" value="查詢">
+    <input type="text"   name="name" value="" placeholder="名稱" required>
+	<input type="text"   name="UserID" value="" placeholder="帳號" required>
+    <input type="text"   name="Passwd" value="" placeholder="密碼" required>
+	<input type="text"   name="Class" value="" placeholder="班級" required>
+    <input type="submit" name="adduserButton" value="新增">
+	
 	
 </form>
-
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $dsn = "mysql:host=localhost;dbname=test";
+ $dsn = "mysql:host=localhost;dbname=test2";
     $db = new PDO($dsn, 'root', '');
-    $name = $_POST['name'];	  //輸入的名稱
-	$account=$_POST['account']; //輸入的帳號
-    $password = $_POST['password']; //輸入的密碼
-	//=========取得查詢結果筆數=======
-	$sql="SELECT * FROM users";	
-	$result=$db->query($sql);
-	$num = $result->rowCount();
-	$num +=1;
-	
-	//================================
-	
-	//==========新增用戶==============
-	if(isset($_POST['addButton'])){
-      $db->query("INSERT INTO users ( id, name,account, password) VALUES ( '$num' ,'$name','$account','$password')"); //新增此資料
-   }
-	if(isset($_POST['deleteButton'])){
-	$db->query("DELETE FROM users WHERE name ='$name' "); //刪除此資料
+    $name = $_POST['name'];	  //輸入使用者的名稱
+    $UserID=$_POST['UserID']; //輸入使用者的帳號
+    $Passwd = $_POST['Passwd']; //輸入使用者的密碼
+	$Class=$_POST['Class'];//輸入使用者班級
+	if(isset($_POST['addtickButton'])){
+	$sql="SELECT * FROM 123";	
+    $result=$db->query($sql);
+    $num = $result->rowCount();
+    $num +=1;
+	$db->query("INSERT INTO 123 ( id, name,UserID, Passwd,Class) VALUES ( '$num' ,'$name','$UserID','$Passwd','$Class')"); 
 	}
-	if(isset($_POST['reviseButton'])){
-	$db->query("UPDATE `users` SET `password` = '$password' WHERE name ='$name' "); //修改此資料
-	}
-	if(isset($_POST['inquireButton'])){
-		$rs=$db->query("SELECT * FROM users where name=$name and account=$account and password=$password");
-		$result= $rs ->fetch();
-		echo '<pre>',print_r($result),'</pre>';
-		}
-	//$sql->execute(); //執行
-	//if (!$result) echo "New OK";
-	//================================
-	
 }
 ?>
-</body>
-</html>
