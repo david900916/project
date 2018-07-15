9a7a39563d5cbd58efa806e5a0853b37a71acf73
david900916 <39638467+david900916@users.noreply.github.com>
2018-07-15 14:15:18 +0800

    Add files via upload

--- HOME.php
+++ HOME.php
@@ -1,12 +1,14 @@
 <?php session_start(); ?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <form name="form" method="post"><CENTER>
-<a href="User Manager AP.php">使用者資料管理</a><P ALIGN=LEFT、CENTER、RIGHT>
+<a href="UserManagerAP.php">使用者資料管理</a><P ALIGN=LEFT、CENTER、RIGHT>
 <a href="Point.php">積分管理</a><P ALIGN=LEFT、CENTER、RIGHT>
 <a href="UserQuery.php">使用者查詢</a><P ALIGN=LEFT、CENTER、RIGHT>
 <a href="Card.php">卡片</a><P ALIGN=LEFT、CENTER、RIGHT>
 <a href="index.php">登入</a><P ALIGN=LEFT、CENTER、RIGHT>
 <a href="logout.php">登出</a><P ALIGN=LEFT、CENTER、RIGHT></CENTER>
 最新消息<P ALIGN=LEFT、CENTER、RIGHT></CENTER>
-
+<?php
+echo "<img src=\"http://www.funhouse.com.tw/wp-content/uploads/2016/10/38194357_m.jpg\">"; 
+?>
 </form>
