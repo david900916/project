5b47232d1f1422cff778236c558f3f12dcaebbf1
david900916 <39638467+david900916@users.noreply.github.com>
2018-07-15 21:23:21 +0800

    Add files via upload

--- message.php
+++ message.php
@@ -1,10 +1,11 @@
 <form name="form" method="post">
 <H1>系統功能更新資訊2018/7/15</H1>
-<CENTER><B>使用者資料管理功能:完全啟用<BR>
-<BR>積分管理功能:尚未開放<BR>
-<BR>使用者查詢功能:尚未開放<BR>
-<BR>登入功能:完全啟用<BR>
-<BR>登出功能:完全啟用<BR><BR></B>
+<CENTER><B><font color="green">使用者資料管理功能:完全開放</font><BR>
+<BR><font color="red">積分管理功能:尚未開放</font><BR>
+<BR><font color="red">使用者查詢功能:尚未開放</font><BR>
+<BR><font color="blue">卡片功能:部分開放</font><BR>
+<BR><font color="green">登入功能:完全開放</font><BR>
+<BR><font color="green">登出功能:完全開放<BR><BR></B>
 <a href="HOME.php">返回首頁</a></CENTER>
 
 </form>
