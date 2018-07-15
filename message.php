dd82e6b8a9b4013fd12a7791950c52190ed76b93
david900916 <39638467+david900916@users.noreply.github.com>
2018-07-15 22:26:06 +0800

    Add files via upload

--- message.php
+++ message.php
@@ -1,11 +1,15 @@
 <form name="form" method="post">
-<H1>系統功能更新資訊2018/7/15</H1>
-<CENTER><B><font color="green">使用者資料管理功能:完全開放</font><BR>
-<BR><font color="red">積分管理功能:尚未開放</font><BR>
-<BR><font color="red">使用者查詢功能:尚未開放</font><BR>
-<BR><font color="blue">卡片功能:部分開放</font><BR>
-<BR><font color="green">登入功能:完全開放</font><BR>
-<BR><font color="green">登出功能:完全開放<BR><BR></B>
-<a href="HOME.php">返回首頁</a></CENTER>
+<H1 align="center">系統功能更新資訊2018/7/15</H1>
+<font color="green"><H2>使用者資料管理功能:完全開放</font></H2>
+<p><b>•使用者資料管理功能:新增帳戶,批輛新增帳戶,修改帳戶,刪除帳戶,查詢帳戶</p></b>
+<font color="red"><H2>積分管理功能:尚未開放</font></H2>
+<font color="red"><H2>使用者查詢功能:尚未開放</font></H2>
+<font color="blue"><H2>卡片功能:部分開放</font></H2>
+<p><b>•卡片功能:僅有表單功能不具實際購買功能</p></b>
+<font color="green"><H2>登入功能:完全開放</font></H2>
+<p><b>•登入功能:會員登入,申請帳戶</p></b>
+<font color="green"><H2>登出功能:完全開放</font></H2>
+<p><b>•登出功能:會員登出</p></B>
+<a href="HOME.php">返回首頁</a>
 
 </form>
